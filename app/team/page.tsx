97d0fc19aeb4e175ec9623fe2c616cc3dd9ec8ea
Sanjana Yadav <126410251+SanjanaYdv1@users.noreply.github.com>
2025-06-06 20:10:15 +0545

    update meta tag

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -40,13 +40,17 @@ const Team = () => {
   return (
     <>
       <Head>
-        <title>Our Team | Kopila Clinic</title>
-        <meta name="description" content="Meet the dedicated team at Kopila Clinic, including doctors, nurses, and staff committed to your care." />
-        <meta property="og:title" content="Our Team | Kopila Clinic" />
-        <meta property="og:description" content="Meet the dedicated team at Kopila Clinic, including doctors, nurses, and staff committed to your care." />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://yourdomain.com/team" />
-        <meta property="og:image" content="https://yourdomain.com/team/team1.png" />
+        <title>Kopila Fertility & Womens Clinic, Damak-6, Jhapa, Nepal</title>
+        <meta name="description" content="Kopila Fertility & Womens Clinic in Damak, Jhapa, Nepal, offers expert gynecological care, fertility treatments, and Womens health services with state-of-the-art facilities and compassionate care."/>
+	      <meta name="keywords" content="jhapa gyne clinic, kopila clinic, damak gyne clinic, women clinic damak, jhapa clinic"/>
+	      <meta name="author" content="PRACAS"/>
+        <meta property="og:url" content="https://kopilaclinic.com.np"/>
+        <meta property="og:title" content="Kopila Fertility & Womens Clinic | Damak, Jhapa, Nepal"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:description" content="Kopila Fertility & Womens Clinic in Damak, Jhapa, Nepal, offers expert gynecological care, fertility treatments, and Womens health services with state-of-the-art facilities and compassionate care."/>
+        <meta property="og:image" content="https://kopilaclinic.com.np/images/og.jpg"/>
+        <meta property="og:image:alt" content="Kopila Fertility & Womens Clinic logo or clinic image"/>
+    
       </Head>
       <div className="bg-[#f3fbff] min-h-screen">
         {/* Animation overlay */}
